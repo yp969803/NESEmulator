9e186a4c86509edb13ecd6c999e02cebb8837e19
YASH PATEL <yp969803@gmail.com>
2024-12-16 13:09:22 +0530

    feat: added address functions

--- source/olc6502.cpp
+++ source/olc6502.cpp
@@ -81,60 +81,218 @@ void olc6502::SetFlag(FLAGS6502 f, bool v){
 	}
 }
 
-// Addressing Mode
-uint8_t olc6502::IMP(){
-	fetched=a;
+///////////////////////////////////////////////////////////////////////////////
+// ADDRESSING MODES
+
+// The 6502 can address between 0x0000 - 0xFFFF. The high byte is often referred
+// to as the "page", and the low byte is the offset into that page. This implies
+// there are 256 pages, each containing 256 bytes.
+//
+// Several addressing modes have the potential to require an additional clock
+// cycle if they cross a page boundary. This is combined with several instructions
+// that enable this additional clock cycle. So each addressing function returns
+// a flag saying it has potential, as does each instruction. If both instruction
+// and address function return 1, then an additional clock cycle is required.
+
+
+// Address Mode: Implied
+// There is no additional data required for this instruction. The instruction
+// does something very simple like like sets a status bit. However, we will
+// target the accumulator, for instructions like PHA
+uint8_t olc6502::IMP()
+{
+	fetched = a;
 	return 0;
 }
 
+
+// Address Mode: Immediate
+// The instruction expects the next byte to be used as a value, so we'll prep
+// the read address to point to the next byte
 uint8_t olc6502::IMM()
 {
-	addr_abs=pc++;
+	addr_abs = pc++;	
 	return 0;
 }
 
+
+
+// Address Mode: Zero Page
+// To save program bytes, zero page addressing allows you to absolutely address
+// a location in first 0xFF bytes of address range. Clearly this only requires
+// one byte instead of the usual two.
 uint8_t olc6502::ZP0()
 {
-	addr_abs=read(pc);
+	addr_abs = read(pc);	
 	pc++;
 	addr_abs &= 0x00FF;
 	return 0;
 }
 
+
+
+// Address Mode: Zero Page with X Offset
+// Fundamentally the same as Zero Page addressing, but the contents of the X Register
+// is added to the supplied single byte address. This is useful for iterating through
+// ranges within the first page.
 uint8_t olc6502::ZPX()
 {
-	addr_abs=(read(pc)+x);
+	addr_abs = (read(pc) + x);
 	pc++;
 	addr_abs &= 0x00FF;
 	return 0;
 }
 
+
+// Address Mode: Zero Page with Y Offset
+// Same as above but uses Y Register for offset
 uint8_t olc6502::ZPY()
 {
-	addr_abs=(read(pc)+x);
+	addr_abs = (read(pc) + y);
 	pc++;
 	addr_abs &= 0x00FF;
 	return 0;
 }
 
-uint8_t olc6502::ABS()
-{
-	uint16_t lo=read(pc);
-	pc++;
-	uint16_t hi=read(pc);
-	pc++;
 
-	addr_abs = (hi<<8) | lo;
+// Address Mode: Relative
+// This address mode is exclusive to branch instructions. The address
+// must reside within -128 to +127 of the branch instruction, i.e.
+// you cant directly branch to any address in the addressable range.
+uint8_t olc6502::REL()
+{
+	addr_rel = read(pc);
+	pc++;
+	if (addr_rel & 0x80)
+		addr_rel |= 0xFF00;
 	return 0;
 }
 
+
+// Address Mode: Absolute 
+// A full 16-bit address is loaded and used
+uint8_t olc6502::ABS()
+{
+	uint16_t lo = read(pc);
+	pc++;
+	uint16_t hi = read(pc);
+	pc++;
+
+	addr_abs = (hi << 8) | lo;
+
+	return 0;
+}
+
+
+// Address Mode: Absolute with X Offset
+// Fundamentally the same as absolute addressing, but the contents of the X Register
+// is added to the supplied two byte address. If the resulting address changes
+// the page, an additional clock cycle is required
 uint8_t olc6502::ABX()
 {
 	uint16_t lo = read(pc);
 	pc++;
-	uint16_t hi=read(pc);
+	uint16_t hi = read(pc);
 	pc++;
-	addr_abs = (hi<<8) | lo;
-	
 
+	addr_abs = (hi << 8) | lo;
+	addr_abs += x;
+
+	if ((addr_abs & 0xFF00) != (hi << 8))
+		return 1;
+	else
+		return 0;	
+}
+
+
+// Address Mode: Absolute with Y Offset
+// Fundamentally the same as absolute addressing, but the contents of the Y Register
+// is added to the supplied two byte address. If the resulting address changes
+// the page, an additional clock cycle is required
+uint8_t olc6502::ABY()
+{
+	uint16_t lo = read(pc);
+	pc++;
+	uint16_t hi = read(pc);
+	pc++;
+
+	addr_abs = (hi << 8) | lo;
+	addr_abs += y;
+
+	if ((addr_abs & 0xFF00) != (hi << 8))
+		return 1;
+	else
+		return 0;
+}
+
+// Note: The next 3 address modes use indirection (aka Pointers!)
+
+// Address Mode: Indirect
+// The supplied 16-bit address is read to get the actual 16-bit address. This is
+// instruction is unusual in that it has a bug in the hardware! To emulate its
+// function accurately, we also need to emulate this bug. If the low byte of the
+// supplied address is 0xFF, then to read the high byte of the actual address
+// we need to cross a page boundary. This doesnt actually work on the chip as 
+// designed, instead it wraps back around in the same page, yielding an 
+// invalid actual address
+uint8_t olc6502::IND()
+{
+	uint16_t ptr_lo = read(pc);
+	pc++;
+	uint16_t ptr_hi = read(pc);
+	pc++;
+
+	uint16_t ptr = (ptr_hi << 8) | ptr_lo;
+
+	if (ptr_lo == 0x00FF) // Simulate page boundary hardware bug
+	{
+		addr_abs = (read(ptr & 0xFF00) << 8) | read(ptr + 0);
+	}
+	else // Behave normally
+	{
+		addr_abs = (read(ptr + 1) << 8) | read(ptr + 0);
+	}
+	
+	return 0;
+}
+
+
+// Address Mode: Indirect X
+// The supplied 8-bit address is offset by X Register to index
+// a location in page 0x00. The actual 16-bit address is read 
+// from this location
+uint8_t olc6502::IZX()
+{
+	uint16_t t = read(pc);
+	pc++;
+
+	uint16_t lo = read((uint16_t)(t + (uint16_t)x) & 0x00FF);
+	uint16_t hi = read((uint16_t)(t + (uint16_t)x + 1) & 0x00FF);
+
+	addr_abs = (hi << 8) | lo;
+	
+	return 0;
+}
+
+
+// Address Mode: Indirect Y
+// The supplied 8-bit address indexes a location in page 0x00. From 
+// here the actual 16-bit address is read, and the contents of
+// Y Register is added to it to offset it. If the offset causes a
+// change in page then an additional clock cycle is required.
+uint8_t olc6502::IZY()
+{
+	uint16_t t = read(pc);
+	pc++;
+
+	uint16_t lo = read(t & 0x00FF);
+	uint16_t hi = read((t + 1) & 0x00FF);
+
+	addr_abs = (hi << 8) | lo;
+	addr_abs += y;
+	
+	if ((addr_abs & 0xFF00) != (hi << 8))
+		return 1;
+	else
+		return 0;
 }
